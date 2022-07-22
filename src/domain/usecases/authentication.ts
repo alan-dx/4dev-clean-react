591c9e671aa9e89439d396b1c82b6af5fe397ac4
Alan Almeida <alanalmeida.emp@gmail.com>
2022-07-22 11:16:20 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,12 @@
+// REGRA DE NEGÓCIO DO LOGIN
+
+import { AccountModel } from '../models/account-model'
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+
+export interface Authentication {
+  auth: (params: AuthenticationParams) => Promise<AccountModel>
+}
