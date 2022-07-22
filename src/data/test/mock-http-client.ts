320b01e5915d843eb28e82379fcf4cea0c78dc0f
Alan Almeida <alanalmeida.emp@gmail.com>
2022-07-22 15:46:48 -0300

    test: ensureRemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,12 +1,17 @@
+import { HttpStatusCode } from './../protocols/http/http-response'
+import { HttpResponse } from '@/data/protocols/http/http-response'
 import { HttpPostParams, HttpPostClient } from '@/data/protocols/http/http-post-client'
 
 export class HttpPostClientSpy implements HttpPostClient {// Spy é um dublê que implementa o protocolo HttpPostClient, o objetivo é testar apenas a unidade RemoteAuthentication
   url?: string
   body?: object
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.noContent
+  }
 
-  async post (params: HttpPostParams): Promise<void> {
+  async post (params: HttpPostParams): Promise<HttpResponse> {
     this.url = params.url
     this.body = params.body
-    return Promise.resolve()
+    return Promise.resolve(this.response)
   }
 }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,7 +1,9 @@
-import { mockAuthentication } from '@/domain/test/mock-autentication'
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { RemoteAuthenticaiton } from './remote-authentication'
+import { HttpPostClientSpy } from '@/data/test/mock-http-client'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import faker from 'faker'
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
+import { mockAuthentication } from '@/domain/test/mock-autentication'
 
 type SutTypes = {
   sut: RemoteAuthenticaiton
@@ -33,5 +35,16 @@ describe('RemoteAuthentication', () => {
     const authenticationParams = mockAuthentication()
     await sut.auth(authenticationParams)
     expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  })
+
+  test('Should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+
+    httpPostClientSpy.response = { // Simulando o erro 401
+      statusCode: HttpStatusCode.unauthorized
+    }
+
+    const promise = sut.auth(mockAuthentication())// Para testar um excessão lançada por uma promise remove o await e armazena em uma variavel
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())// .rejects captura o reject da promise
   })
 })
