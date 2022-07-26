7cf6de3bcfea279c225473edbcc1477f59f05a0e
Alan Almeida <alanalmeida.emp@gmail.com>
2022-07-26 16:23:11 -0300

    chore: config webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')//Recomendação da comunidade
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),//path onde será gerado o bundle
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],//Formato de arquivos que serão tratados pelo webpack
+    alias: {
+      '@': path.join(__dirname, 'src')//"Mapeando o @, ou seja, indicando para o webpack pra qual local o @ deve apontar"
+    }
+  },
+  devServer: {//devServer é quem vai rodar o servidor de desenvolvimento com a aplicação rodando
+    contentBase: './public',//Indicando qual a pasta que contém a base do projeto, ou seja, o local que contém os arquivos compilados que dever ser utilizado pelo devServer
+    writeToDisk: true,//Escreve o bundle.js no disco ao invés de apenas na memória (default)
+    historyApiFallback: true, //Para as diferentes rotas funcionarem
+  },
+  externals: {//Informando quais dependências serão importadas externamente, ou seja, não compiladas para o bundle
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
