4cb4ed538c15242ce8ed77d5ea517458040051d7
Alan Almeida <alanalmeida.emp@gmail.com>
2022-07-31 19:10:32 -0300

    feat: ensure Login will not load loading message and error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Styles from './form-status-styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = React.useContext(Context)
+
   return (
-    <div className={Styles.errorWrap}>
-      <span className={Styles.error}>Error</span>
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
+      {isLoading && (<span className={Styles.error} >Carregando...</span>)}
+      {errorMessage && (<span className={Styles.error}>{errorMessage}</span>)}
     </div>
   )
 }
