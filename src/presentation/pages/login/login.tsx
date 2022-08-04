50f941a1d6f92eb12d57d80c138606e2f17cd417
Alan Almeida <alanalmeida.emp@gmail.com>
2022-08-04 09:57:49 -0300

    feat: ensure Login navigate to main page on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,7 +4,7 @@ import { Footer, LoginHeader, FormStatus, Input } from '@/presentation/component
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
 import { Authentication } from '@/domain/usecases'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 type Props = {
   validation: Validation
@@ -12,6 +12,7 @@ type Props = {
 }
 
 const Login: React.FC<Props> = (props) => {
+  const history = useHistory()
   const [state, setState] = React.useState({
     isLoading: false,
     email: '',
@@ -38,6 +39,7 @@ const Login: React.FC<Props> = (props) => {
       setState({ ...state, isLoading: true })
       const account = await props.authentication.auth({ email: state.email, password: state.password })
       localStorage.setItem('accessToken', account.accessToken)
+      history.replace('/')
     } catch (error) {
       setState({ ...state, isLoading: false, mainError: error.message })
     }
