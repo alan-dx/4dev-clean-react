84c205996e2b2db5fe30e8b00548a6659981d2b2
Alan Almeida <alanalmeida.emp@gmail.com>
2023-01-24 23:56:06 -0300

    refactor: rename data mocks

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,4 +1,10 @@
 import { HttpPostParams, HttpPostClient, HttpResponse, HttpStatusCode } from '@/data/protocols/http'
+import faker from 'faker'
+
+export const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement()
+})
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {// Spy é um dublê que implementa o protocolo HttpPostClient, o objetivo é testar apenas a unidade RemoteAuthentication
   url?: string
