560280482baf2c090d507bd6240ed6116d8c3ad8
Alan Almeida <alanalmeida.emp@gmail.com>
2023-03-06 22:13:40 -0300

    test: ensure RemoteLoadSurveyList calls HttpGetClient with correct URL

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,4 +1,5 @@
-import { HttpPostParams, HttpPostClient, HttpResponse, HttpStatusCode } from '@/data/protocols/http'
+/* eslint-disable @typescript-eslint/require-await */
+import { HttpPostParams, HttpPostClient, HttpResponse, HttpStatusCode, HttpGetClient, HttpGetParams } from '@/data/protocols/http'
 import faker from 'faker'
 
 export const mockPostRequest = (): HttpPostParams => ({
@@ -17,5 +18,12 @@ export class HttpPostClientSpy<R> implements HttpPostClient<R> {// Spy é um dub
     this.url = params.url
     this.body = params.body
     return Promise.resolve(this.response)
+  }
+}
+
+export class HttpGetClientSpy implements HttpGetClient {
+  url: string
+  async get(params: HttpGetParams): Promise<void> {
+    this.url = params.url
   }
 }
